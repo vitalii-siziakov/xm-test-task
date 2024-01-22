8fbdceaf44a89670e88ecbc17f0a6460adf5ddd4
Vitalii Siziakov <vitalii.siziakov@gmail.com>
2024-01-22 23:19:21 +0300

    fix: resolve html syntax error

--- src/components/BrokerFeatures/components/FeatureItem/FeatureItem.js
+++ src/components/BrokerFeatures/components/FeatureItem/FeatureItem.js
@@ -2,11 +2,11 @@ const FeatureItem = ({ item }) => {
   const { title, description, image, alt } = item;
 
   return (
-    <div className='broker-feature__item'>
+    <li className='broker-feature__item'>
       <img className='broker-feature__img' src={image} alt={alt} />
       <h3 className='broker-feature__title'>{title}</h3>
       <p className='broker-feature__description'>{description}</p>
-    </div>
+    </li>
   );
 };
 
